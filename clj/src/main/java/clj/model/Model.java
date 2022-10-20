945e7585c7150777fde35c4d665c848619cba931
chi_chunho <chunho1961@gmail.com>
2022-10-21 00:49:37 +0800

    Modified Model.java

--- clj/src/main/java/clj/model/Model.java
+++ clj/src/main/java/clj/model/Model.java
@@ -2,4 +2,13 @@ package clj.model;
 
 public class Model {
     
+    Board board;
+
+    public Model(){
+        board = new Board();
+    }
+
+    public Response run(Request request){
+        
+    }
 }
