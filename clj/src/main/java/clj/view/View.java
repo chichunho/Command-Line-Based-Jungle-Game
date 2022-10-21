4a9c10e224d92168dfcd9330efcca620d3be852d
chi_chunho <chunho1961@gmail.com>
2022-10-21 10:29:32 +0800

    Modified View.java

--- clj/src/main/java/clj/view/View.java
+++ clj/src/main/java/clj/view/View.java
@@ -7,13 +7,19 @@ public class View {
 
     private String[] messageArray;
 
-    private void readfile(File) {
+    
+    private void readfile(File fpath) {
+        /*
         Scanner Reader = new Scanner(File);
         int x = 0;
         while (Reader.hasNextLine()) {
             this.messageArray[x] = Reader.nextLine();
             x++;
         }
+        */
+        /*
+        TODO add expcptions for file reading process
+        */
     }
 
     public void updateView(){
