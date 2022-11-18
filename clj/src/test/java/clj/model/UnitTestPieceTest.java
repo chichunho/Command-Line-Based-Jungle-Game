7140fcb4ac7c6d49b555f12468af2491761a9b08
chichunho <chunho1961@gmail.com>
2022-11-18 09:26:57 +0800

    add tests

--- clj/src/test/java/clj/model/UnitTestPieceTest.java
+++ clj/src/test/java/clj/model/UnitTestPieceTest.java
@@ -38,11 +38,64 @@ public class UnitTestPieceTest {
         p2Pieces[7] = new RatTest(2);
     }
 
-    /* Rat */
+    /**
+     * Functionality:
+     * This is testing the method inherit form the parent class
+     */
+    @Test
+    public void commonTest(){
+        String[] animalName = {"Elephant", "Lion", "Tiger", "Leopard", "Wolf", "Dog", "Cat", "Rat"};
+        int[] animalRank = {8, 7, 6, 5, 4, 3, 2, 1};
+        for (int i = 0; i < 8; i++){
+            assertEquals(animalName[i], p1Pieces[i].getAnimal());
+            assertEquals(animalName[i], p2Pieces[i].getAnimal());
+            assertEquals(animalRank[i], p1Pieces[i].getRank());
+            assertEquals(animalRank[i], p2Pieces[i].getRank());
+            assertEquals(1, p1Pieces[i].getParty());
+            assertEquals(2, p2Pieces[i].getParty());
+            p1Pieces[i].setTrapped(true);
+            p1Pieces[i].setInWater(true);
+            p2Pieces[i].setTrapped(true);
+            p2Pieces[i].setInWater(true);
+            assertEquals(true, p1Pieces[i].isTrapped());
+            assertEquals(true, p1Pieces[i].isInWater());
+            assertEquals(true, p2Pieces[i].isTrapped());
+            assertEquals(true, p2Pieces[i].isInWater());
+        }
+    }
+
+    /**
+     * Functionality:
+     * This is testing if the canCapture method is work as we designed.
+     * Except rat, all pieces have the same canCapture method implementation
+     */
+    @Test
+    public void canCaptureTest(){
+        for (int i = 0; i < 7; i++){
+            for (int j = i; j < 8; j++){
+                assertEquals(0, p1Pieces[i].canCapture(p2Pieces[j]));
+                assertEquals(0, p1Pieces[i].canCapture(null));
+                assertEquals(1, p1Pieces[i].canCapture(p1Pieces[j]));
+            }
+        }
+
+        for (int i = 7; i > 0; i--){
+            for (int j = i-1; j > 0; j--){
+                assertEquals(2, p1Pieces[i].canCapture(p2Pieces[j]));
+            }
+        }
+
+        for (int i = 7; i > 0; i--){
+            for (int j = i-1; j > 0; j--){
+                p2Pieces[j].setTrapped(true);
+                assertEquals(0, p1Pieces[i].canCapture(p2Pieces[j]));
+            }
+        }
+    }
 
     /**
      * Functionalilty:
-     * This is testing if the canCapture method work as we designed
+     * This is testing if the canCapture method work as we designed (rat only)
      */
     @Test
     public void ratCanCaptureTest(){
@@ -73,7 +126,7 @@ public class UnitTestPieceTest {
 
     /**
      * Functionality:
-     * This is testing if canMoveTo method work as we designed
+     * This is testing if canMoveTo method work as we designed (rat only)
      */
     @Test
     public void ratCanMoveToTest(){
@@ -86,78 +139,39 @@ public class UnitTestPieceTest {
         assertEquals(0, p1rat.canMoveTo(dummy2));
     }
 
-    /* Since the canCapture and canMoveTo methods implementation of 
-     * Cat, Dog, Leopard, Wolf are the same,
-     * here we just use one of them to test
-    */
-
-    /**
-     * Functionality:
-     * This is testing if the canCapture method work as we designed
-     */
+     /**
+      * Functionality:
+      * This is testing if the calFinalDest work as we designed (jump animals only)
+      */
     @Test
-    public void catCanCaptureTest(){
-        PieceTest p1cat = p1Pieces[6];
-        PieceTest p2elephant = p2Pieces[0];
-        PieceTest p1rat = p1Pieces[7];
-        // testing if branch -> if (another == null)
-        assertEquals(0, p1cat.canCapture(null));
-        // tetsing if branch -> if (another.getParty() == this.getParty())
-        assertEquals(1, p1cat.canCapture(p1rat));
-        // testing if branch -> if (this.getRank() < another.getRank() &&
-        //                      !another.isTrapped())
-        assertEquals(2, p1cat.canCapture(p2elephant));
-    }
-
-    /**
-     * Functionality:
-     * This is testing if canMoveTo method work as we designed
-     */
-    @Test
-    public void catCanMoveToTest(){
-        BoardObjTest waterSquare = new BoardObjTest("Water", 0);
-        BoardObjTest ownDen = new BoardObjTest("Den", 1);
-        PieceTest p1cat = p1Pieces[6];
-        // testing if branch -> if (dest.getType().equals("Den") &&
-        //                      dest.getParty() == this.getParty())
-        assertEquals(1, p1cat.canMoveTo(ownDen));
-        // testing if branch -> if (dest.getType().equals("Water"))
-        assertEquals(2, p1cat.canMoveTo(waterSquare));
-    }
-
-    /* For lion and tiger, since they can jump over river,
-     * they have a different calFinalDest implementation compared to other piece,
-     * since the implementation of calFinalDest is the same in lion and tiger,
-     * here we just test one of them
-    */
-
-    /**
-     * Functionality:
-     * This is testing if the calFinalDest work as we designed
-     */
-     @Test
-     public void lionCalFinalDestTest(){
+    public void lionTigerCalFinalDestTest(){
         board = new BoardTest();
         board.testEmptyBoardPieces();
-
-        // normal jump action
         PieceTest p1lion = p1Pieces[1];
-        Coordinate expectedDest = new Coordinate("D6");
+        PieceTest p1tiger = p1Pieces[2];
+        PieceTest[] jumpAnimals = {p1lion, p1tiger};
         Coordinate dest = new Coordinate("B6");
-        dest = p1lion.calFinalDest(dest, board, 1, 0);
-        assertEquals(dest.getCol(), expectedDest.getCol());
-        assertEquals(dest.getRow(), expectedDest.getRow());
+        Coordinate expectedDest;
+
+        for (int i = 0; i < 2; i++){
+            // normal jump action
+            expectedDest = new Coordinate("D6");
+            dest = jumpAnimals[i].calFinalDest(dest, board, 1, 0);
+            assertEquals(dest.getCol(), expectedDest.getCol());
+            assertEquals(dest.getRow(), expectedDest.getRow());
+        }
+        
 
         // insert a rat in the mid-way
         PieceTest p1rat = p1Pieces[0];
         Coordinate p1ratPos = new Coordinate("C6");
         board.testSetPiece(p1rat, p1ratPos);
-        dest = new Coordinate("B6");
-        dest = p1lion.calFinalDest(dest, board, 1, 0);
-        // the dest should be "C6", which means lion is block by the rat
-        assertEquals(dest.getCol(), p1ratPos.getCol());
-        assertEquals(dest.getRow(), p1ratPos.getRow());
-     }
-
-    
+        for(int i = 0; i < 2; i++){
+            dest = new Coordinate("B6");
+            dest = p1lion.calFinalDest(dest, board, 1, 0);
+            // the dest should be "C6", which means lion is block by the rat
+            assertEquals(dest.getCol(), p1ratPos.getCol());
+            assertEquals(dest.getRow(), p1ratPos.getRow());
+        }
+    }   
 }
